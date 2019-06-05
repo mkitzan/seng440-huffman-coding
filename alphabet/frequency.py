3e090b76fd54c63029e70e1a3796ec11ab26e62c
Michael Kitzan <mkitzan45@gmail.com>
2019-06-04 22:08:29 -0700

    Frequency cmd line argument 2 and TODO

--- alphabet/frequency.py
+++ alphabet/frequency.py
@@ -8,7 +8,7 @@ def proc_file(file, data):
     with open(argv[1] + '/' + file, 'r', encoding='latin-1') as file:
         for line in file:
             for c in line:
-                data[ord(c)] += 1
+                data[ord(c) % int(argv[2])] += 1
                 count += 1
                 
     return count
@@ -29,15 +29,16 @@ def save_data(stats):
 
 
 def main():
-    if len(argv) == 1:
-        print("Must provide folder location files to analyze")
+    if len(argv) != 3:
+        print("Argument 1: must provide folder location of files to analyze")
+        print("Argument 2: must provide a modulo size 128 or 256")
         return
     
     total = 0
     count = 0
-    stats = [0] * 128
+    stats = [0] * int(argv[2])
     for file in sorted(listdir(argv[1])):
-        data = [0] * 128
+        data = [0] * int(argv[2])
         count = proc_file(file, data)
         total += count
         record_stats(data, count, stats)
