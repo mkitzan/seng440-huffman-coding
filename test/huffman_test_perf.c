d347c9abb75df9d835bea5938159cba31eef4163
Michael Kitzan <mkitzan45@gmail.com>
2019-07-07 14:07:19 -0700

    Small test change

--- test/huffman_test_perf.c
+++ test/huffman_test_perf.c
@@ -4,14 +4,13 @@
 #include "../src/huffman.h"
 
 int main() {
-    register unsigned int n;
     uint32_t result[CODE_SIZE];
     
     // set up statically allocated huffman tree
     build();
     
-    n = encode(PLAIN_TEXT, result);
-    n = decode(result, PLAIN_TEXT);
+    encode(PLAIN_TEXT, result);
+    decode(result, PLAIN_TEXT);
     
     return 0;
 }
