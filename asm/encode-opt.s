4e9e752f298ff74b688075e2dc8ba42b2f4eee79
Michael Kitzan <mkitzan45@gmail.com>
2019-07-17 11:04:32 -0700

    Fixed bugs with optimized decode asm function

--- asm/encode-opt.s
+++ asm/encode-opt.s
@@ -5,27 +5,27 @@ encode:
     push   {r4, r5, r6, r7, r8, r9, lr}
     ldrb   r3, [r0] ; [zero_extendqisi2] load r3 (key) with first char in text
     movt   r2, #:upper16:DICT ; r2 = DICT addr
-    mov    r4, r1 ; copy code addr
+    mov    r4, r1, lsr #2 ; copy code addr
     mov    r5, #0 ; len = 0
     mov    r6, #0 ; buffer = 0
-.L3
+    ldr    r7, [r2] ; derefence DICT
+.L3:
     cmp    r3, #0 ; text[i] == 0
-    add	   r3, r3, r3, lsl #1 ; turn key into index offset for DICT
+    add    r3, r3, r3, lsl #1 ; turn key into index offset for DICT
     beq    .L4
-    add    r3, r2, r3 ; get DICT[key] addr
-    add    r0, r0, #1 ; text = text + 1
-    ldrh   r8, [r3] ; load DICT[key].code
-    ldrb   r9, [r3, #2] ; load DICT[key].len
+    add    r3, r7, r3 ; get DICT[key] addr
+    ldrh   r8, [r3] ; [unaligned] load DICT[key].code
+    ldrb   r9, [r3, #2] ; [zero_extendisi2] load DICT[key].len
     orr	   r6, r6, r8, lsl r5 ; buffer |= DICT[key].code << len
     add    r5, r5, r9 ; len += DICT[key].len
-    ldrb   r3, [r0] ; load text[i]
+    ldrb   r3, [r0, #1]! ; [zero_extendqisi2] load text[i]
     tst	   r5, #32 ; len & CODE
-    streq  r6, [r1], #4 ; code[loc] = buffer, code = code + 1
-    subeq  r5, #32 ; len -= CODE
-    subeq  r9, r5 ; DICT[key].len - len 
-    lsreq  r6, r8, r9 ; buffer = DICT[key].code >> (DICT[key].len - len)
+    strne  r6, [r1], #4 ; code[loc] = buffer, code = code + 1
+    subne  r5, r5, #32 ; len -= CODE
+    subne  r9, r9, r5 ; DICT[key].len - len 
+    lsrne  r6, r8, r9 ; buffer = DICT[key].code >> (DICT[key].len - len)
     b      .L3
-.L4
-    str    r6, [r1], #1 ; store fence post buffer
-    sub    r0, r1, r4 ; return text[loc] - text
+.L4:
+    str    r6, [r1] ; store fence post buffer
+    rsb    r0, r4, r1, lsr #2 ; return text[loc] - text
     pop    {r4, r5, r6, r7, r8, r9, pc}
--- asm/huffman-lu-opt.s
+++ asm/huffman-lu-opt.s
@@ -65,13 +65,14 @@ decode:
     mov   ip, #0 @ ip is seen
     ldr   r3, [r0] @ r3 is buffer
     mov   r2, r1 @ r2 = text addr
+    mov   r9, r3
     movt  r8, #:upper16:LOOKUP @ r8 is temp var storing LOOKUP addr
 .L12:
-    and   lr, r3, #7 @ draw = buffer & 7
+    and   lr, r9, #7 @ draw = buffer & 7
     ldr   r0, [r8] @ r0 = LOOKUP addr
     add   lr, lr, lr, lsl #2 @ turn draw into index offset for LOOKUP
     add   r0, r0, lr @ get LOOKUP[draw] addr
-    lsr   r9, r3, #3 @ buffer >>= 3
+    lsr   r9, r9, #3 @ buffer >>= 3
     ldrb  r4, [r0, #4] @ load LOOKUP[draw].draw
     ldr   lr, [r0] @ load LOOKUP[draw].table
     and   r4, r4, r9 @ draw &= buffer
@@ -79,19 +80,19 @@ decode:
     ldrb  r6, [lr] @ load table[draw].letter
     ldrb  lr, [lr, #1] @ load table[draw].contrib
     strb  r6, [r2], #1 @ text[loc] = temp@
-    ldr	  r3, [r7, #4]! @ load code[pos]
-    add	  r0, lr, #3 @ bits = table[draw].contrib + 3
+    lsr   r9, r9, lr @ buffer >>= table[draw].contrib
+    add   r0, lr, #3 @ bits = table[draw].contrib + 3
     lsr   r3, r3, ip @ draw = code[pos] >> seen
     rsb   r4, r0, #32 @ CODE - bits
     add   ip, ip, r0 @ seen += bits
     lsl   r3, r3, r4 @ draw << (CODE - bits) 
     tst   ip, #32 @ if(seen & CODE)
-    orr   r3, r3, r9, lsr lr @ buffer |= draw << (CODE - bits)
+    orr   r9, r9, r3, lsr l4 @ buffer |= draw << (CODE - bits)
     beq   .L11
     subs  ip, ip, #32 @ seen -= CODE and if(seen)
+    ldrne r3, [r7, #4]! @ if(seen) load code[pos]
     rsbne r4, ip, #32 @ if(seen) CODE - seen
-    ldrne r9, [lr, #4]! @ if(seen) load code[pos]
-    orrne r3, r3, r9, lsl r4 @ if(seen) buffer |= code[pos] << (CODE - seen)
+    orrne r9, r9, r3, lsl r4 @ if(seen) buffer |= code[pos] << (CODE - seen)
 .L11:
     cmp   r6, #3 @ letter != 0x03
     sub   r0, r2, r1 @ return loc = (text[loc] - text)
